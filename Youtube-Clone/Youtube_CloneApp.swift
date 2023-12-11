417049265821a7901b0b475e680ea2e995ffc4da
Nitish Kumar <nitish.kumar@mindfiresolutions.com>
2023-12-11 20:57:55 +0530

    Initial Commit

---
+++ Youtube-Clone/Youtube_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_CloneApp.swift
+//  Youtube-Clone
+//
+//  Created by Nitish Kumar on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
